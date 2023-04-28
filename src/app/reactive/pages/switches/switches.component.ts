481784ff9905e8a60503a277341448b6f7dfb311
AndyCastro16 <c.castro@dappertechnologies.com>
2023-04-28 17:37:51 -0500

    ADD: Switches Component added
    Se añadio el componente de switches, con el cual se probo la funcionalidad de los Reactive Forms con switches de varios tipos

--- src/app/reactive/pages/switches/switches.component.ts
+++ src/app/reactive/pages/switches/switches.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './switches.component.html',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   ]
 })
 export class SwitchesComponent {
+  public myForm = this.fb.group({
+    gender: ['', Validators.required],
+    wantNotifications: [ false, Validators.required ],
+    termsAndConditions: [ false, Validators.requiredTrue ]
+  })
 
+  constructor(
+    public fb: FormBuilder
+  ){
+
+  }
+
+  onSave(){
+
+    if( this.myForm.invalid ){
+      this.myForm.markAllAsTouched();
+      return;
+    }
+    this.myForm.reset()
+  }
 }
