6c5aab13e2a363a41e02cb233aab0e1c66f80566
AndyCastro16 <c.castro@dappertechnologies.com>
2023-04-28 17:07:52 -0500

    ADD: Basic forms and Dynamic Forms Added
    Se realizaron las paginas de basic forms y tambien las Dynamic forms

--- src/app/reactive/pages/basic/basic.component.ts
+++ src/app/reactive/pages/basic/basic.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './basic.component.html',
   styles: [
   ]
 })
-export class BasicComponent {
+export class BasicComponent implements OnInit{
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    price: [0, [Validators.required, Validators.min(0)]],
+    inStorage: [0, [Validators.required, Validators.min(0)]]
+  })
+  constructor(private fb: FormBuilder){}
 
+  ngOnInit(): void {
+    
+  }
+
+  isValidField(field: string){
+    return this.myForm.controls[field].errors 
+      && this.myForm.controls[field].touched
+  }
+  getFieldError(field: string): string | null{
+    if( !this.myForm.controls[field] ) return null;
+    const errors = this.myForm.controls[field].errors || {};
+    for(const key of Object.keys(errors)){
+      switch(key){
+        case 'required':
+          return 'This field is required';
+        case'minlength':
+          return 'This field must be at least 3 characters long'
+        case 'min':
+          return 'This field must be greater than or equal to 0'
+      }
+    }
+    return null
+  }
+  onSave(){
+    let formValid = this.myForm.invalid
+    if(formValid) return;
+    console.log(this.myForm.value);
+    this.myForm.reset();
+  }
 }
--- src/app/reactive/pages/dynamic/dynamic.component.ts
+++ src/app/reactive/pages/dynamic/dynamic.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './dynamic.component.html',
@@ -6,5 +7,71 @@ import { Component } from '@angular/core';
   ]
 })
 export class DynamicComponent {
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    favoriteGames: this.fb.array([
+      ['Metal Gear', Validators.required],
+      ['Chess', Validators.required]
+    ])
+  })
+  public newFavorite = new FormControl('', [Validators.required])
+  constructor(
+    private fb: FormBuilder
+  ){
 
+  }
+  get favoriteGames(){
+    return this.myForm.get('favoriteGames') as FormArray;
+
+  }
+  isValidField( field: string ): boolean | null {
+    return this.myForm.controls[field].errors
+      && this.myForm.controls[field].touched;
+  }
+
+  isValidFieldInArray( formArray: FormArray, index: number ) {
+    return formArray.controls[index].errors
+        && formArray.controls[index].touched;
+  }
+  getFieldError( field: string ): string | null {
+
+    if ( !this.myForm.controls[field] ) return null;
+
+    const errors = this.myForm.controls[field].errors || {};
+
+    for (const key of Object.keys(errors) ) {
+      switch( key ) {
+        case 'required':
+          return 'Este campo es requerido';
+
+        case 'minlength':
+          return `Mínimo ${ errors['minlength'].requiredLength } caracters.`;
+      }
+    }
+
+    return null;
+  }
+  onAddFavorites(){
+    if( this.newFavorite.invalid) return
+    const newGame = this.newFavorite.value;
+
+    this.favoriteGames.push(
+      this.fb.control(newGame, Validators.required)
+    );
+    
+    this.newFavorite.reset();
+    console.log(this.newFavorite.value);
+  }
+  onDeleteFavorite(index: number): void{
+    this.favoriteGames.removeAt(index);
+  }
+  onSubmit(){
+    if(this.myForm.invalid){
+      this.myForm.markAllAsTouched();
+      return;      
+    }
+    console.log('Reiniciado');
+    (this.myForm.controls['favoriteGames'] as FormArray) = this.fb.array([])
+    this.myForm.reset();
+  }
 }
